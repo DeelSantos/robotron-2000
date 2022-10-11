95ce6a7b233b06b3931bb59b36b9ba7196af3345
Deel Santos <edelsonjr.4@hotmail.com>
2022-10-11 10:47:22 -0300

    Trabalhando com JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-const robotron = document.querySelector('#robotron');
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-function dizOi() {
-    console.log('oi');
+const controle = document.querySelectorAll('.controle-ajuste');
+const contador = document.querySelectorAll('.controle-contador');
+
+
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+
+
+function manipulaDados(operacao) {
+    if (operacao === '-' ) {
+        contador.value = parseInt(contador.value) - 1
+    } else {
+        contador.value = parseInt(contador.value) + 1
+    }
 }
 
-robotron.addEventListener('click', dizOi);
+
